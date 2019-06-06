d5e57f6aad0d654b335626e5318b41f4e8135b39
adityamudgil2505 <adityamudgil2505@gmail.com>
2019-06-07 00:16:11 +0530

    Toggle button with ngIf directive

--- first-project/src/app/test/test.component.ts
+++ first-project/src/app/test/test.component.ts
@@ -17,8 +17,20 @@ import { $ } from 'protractor';
   }`]
 })
 export class TestComponent implements OnInit {
-  public inputText:string="";
+  // public inputText:string="";
+  public isChecked:Boolean = false;
+  public btnContent:string = "Show";
   constructor() { }
   ngOnInit() {
+  }
+  onClick(){
+    if(this.btnContent=="Show"){ 
+      this.isChecked=true;
+      this.btnContent="Hide";
+    }
+    else{ 
+      this.isChecked=false;
+      this.btnContent="Show";
+    }
   }
 }
